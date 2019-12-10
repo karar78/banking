70fc55df9667d960149be4d475fdc42fc82b9e5a
karar78 <karar@live.com>
2019-12-10 02:01:01 +0000

    indentation

--- banking/src/test/java/com/fractal/banking/BankingControllerTest.java
+++ banking/src/test/java/com/fractal/banking/BankingControllerTest.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import com.fractal.banking.controller.BankingController;
 
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(BankingController.class)
 class BankingControllerTest {
@@ -25,31 +23,27 @@ class BankingControllerTest {
 
 	@Test
 	void testGetBanks() throws Exception {
-		mockMvc.perform(get("/fractal/banks"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("/all-banks"))
-		.andDo(MockMvcResultHandlers.print())
-		.andExpect(content().string(org.hamcrest.Matchers.containsString("HSBC")));
+		mockMvc.perform(get("/fractal/banks")).andExpect(status().isOk()).andExpect(view().name("/all-banks"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(content().string(org.hamcrest.Matchers.containsString("HSBC")));
 	}
-	
+
 	@Test
 	void testTransactions() throws Exception {
-		String bankId="2";
-		String accountId="fakeAcc102";
+		String bankId = "2";
+		String accountId = "fakeAcc102";
 		mockMvc.perform(get("/fractal/banking/" + bankId + "/accounts/" + accountId + "/transactions"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("/transactions"))
-		.andDo(MockMvcResultHandlers.print())
-		.andExpect(content().string(org.hamcrest.Matchers.containsString("0ef942ea-d3ad-4f25-857b-4d4bb7f912d8")));
+				.andExpect(status().isOk()).andExpect(view().name("/transactions")).andDo(MockMvcResultHandlers.print())
+				.andExpect(
+						content().string(org.hamcrest.Matchers.containsString("0ef942ea-d3ad-4f25-857b-4d4bb7f912d8")));
 	}
-	
+
 	@Test
 	void testCategorizedTransactions() throws Exception {
 		String companyId = "2";
 		mockMvc.perform(get("/fractal/banking/categories/transactions?companyId=" + companyId))
-		.andExpect(status().isOk())
-		.andExpect(view().name("/detailed-transactions"))
-		.andDo(MockMvcResultHandlers.print())
-		.andExpect(content().string(org.hamcrest.Matchers.containsString("fakeTrx09")));
+				.andExpect(status().isOk()).andExpect(view().name("/detailed-transactions"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(content().string(org.hamcrest.Matchers.containsString("fakeTrx09")));
 	}
 }
